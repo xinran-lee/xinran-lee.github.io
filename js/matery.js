f5da22741247c54a4c18e53d0036d590e5f290ac
xinran-lee <xinranlee8@gmail.com>
2022-03-26 15:45:47 +0800

    Site updated: 2022-03-26 15:45:45

--- js/matery.js
+++ js/matery.js
@@ -57,6 +57,18 @@ $(function () {
         fixStyles();
     });
 
+
+    /*黑夜模式提醒开启功能*/
+    //黑夜模式提醒开启功能
+    setTimeout(function () {
+        if ((new Date().getHours() >= 19 || new Date().getHours() < 7) && !$('body').hasClass('DarkMode')) {
+            let toastHTML = '<span style="color:#97b8b2;border-radius: 10px;>' + '<i class="fa fa-bellaria-hidden="true"></i>晚上使用黑夜模式阅读能够减轻视觉疲劳。</span>'
+            M.toast({ html: toastHTML })
+        }
+    }, 2000)
+    /*黑夜模式提醒开启功能*/
+
+
     /*初始化瀑布流布局*/
     $('#articles').masonry({
         itemSelector: '.article'
